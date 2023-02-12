e506e390befd0ae0e44ce490b191fb9594657d7a
Sergey Zhirnov <mahorishe73@gmail.com>
2023-02-12 13:54:18 +0300

    Refactoring v.2

---
+++ src/components/counter.jsx
@@ -0,0 +1,36 @@
+const Counter = (props) => {
+  const { value, onDelete, onIncrement, onDecrement } = props;
+
+  const formatValue = () => {
+    return value === 0 ? "empty" : value;
+  };
+  const getBageClasses = () => {
+    let classes = "badge m-2 ";
+    classes += value === 0 ? "bg-warning" : "bg-primary";
+    return classes;
+  };
+
+  return (
+    <div>
+      <span> {props.name}</span>
+      <span className={getBageClasses()}>{formatValue()}</span>
+      <button
+        className="btn btn-primary btn-sm m-2"
+        onClick={() => onIncrement(props.id)}>
+        +
+      </button>
+      <button
+        className="btn btn-primary btn-sm m-2"
+        onClick={() => onDecrement(props.id)}>
+        -
+      </button>
+      <button
+        className="btn btn-danger btn-sm m-2"
+        onClick={() => onDelete(props.id)}>
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
